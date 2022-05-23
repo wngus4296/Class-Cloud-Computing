05979805655dce3a1d4d87e133bfa3a9bb027707
juhyeon <wngus4296@swu.ac.kr>
2022-05-24 02:41:09 +0900

    [FEAT] DB, DB2 결과 모두 보여주도록 구현

--- VagrantMultiAdditionalFiles/show.php
+++ VagrantMultiAdditionalFiles/show.php
@@ -4,18 +4,21 @@
 $username = 'root';
 $password = 'rootpass';
 $dsn = 'mysql:host=192.168.3.6;dbname=formresponses';
+$dsn2 = 'mysql:host=192.168.3.7;dbname=formresponses';
 
 try{
-	$db = new PDO($dsn, $username, $password);
-	$result=FALSE;
+   $db = new PDO($dsn, $username, $password);
+   $db2 = new PDO($dsn2, $username, $password);
+   $result=FALSE;
 
     $query = "SELECT * FROM response";
 
     $stmt = $db->query($query);
+    $stmt2 = $db2->query($query);
     
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    
+    $rows2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+  
     echo "<table border='1'>
     <tr>
     <th>Firstname</th>
@@ -33,8 +36,25 @@ try{
         //printf("{$row['firstname']} {$row['secondname']} {$row['email']}\n");
     }
 
+    echo "<table border='1'>
+    <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>email</th>
+    </tr>";
+
+    foreach($rows2 as $row) {
+        echo "<tr>";
+        echo "<td>".$row['firstname']."</td>";
+        echo "<td>".$row['lastname']."</td>";
+        echo "<td>".$row['email']."</td>";
+        echo "</tr>";
+
+        //printf("{$row['firstname']} {$row['secondname']} {$row['email']}\n");
+    }
+
 } catch(PDOException $ex) {
-	echo $ex->getMessage();
+   echo $ex->getMessage();
 }
 
 echo "</table>";
